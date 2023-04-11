0d0a46e538d7b4b07e469ab9b9c529425029578e
SumbulIshdavletova <ishdavletova.sumbul@gmail.com>
2023-04-11 20:20:34 +0500

    Initial commit

---
+++ app/src/main/java/ru/sumbul/rickandmorty/ui/MainActivity.kt
@@ -0,0 +1,43 @@
+package ru.sumbul.rickandmorty.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.fragment.app.Fragment
+import ru.sumbul.rickandmorty.R
+import ru.sumbul.rickandmorty.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen() //должна быть первая иначе все сломается
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        replaceFragment(CharactersListFragment())
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.characters -> replaceFragment(CharactersListFragment())
+                R.id.episodes -> replaceFragment(EpisodesListFragment())
+                R.id.locations -> replaceFragment(LocationsListFragment())
+
+                else -> {
+                }
+            }
+            true
+
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+}
